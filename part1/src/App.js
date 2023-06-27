6e19a90714c18f1da3b67e751ef192d58f958f0a
atayiilmaz <ataberkyiilmaz@outlook.com>
2023-06-27 19:33:05 +0300

    minor changes on app.js

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+const App = () => {
+
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
+
+
+  return (
+    
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+
+  );
+}
 
 export default App;
