6f54c250cd985559eaf724e1fa32192cbb9a5d1e
atayiilmaz <ataberkyiilmaz@outlook.com>
2023-06-28 19:36:25 +0300

    added footer component

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,22 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-const Hello = () => {
+const Hello = (props) => {
+
+  console.log(props)
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/atayiilmaz'>atayiilmaz</a>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
+
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
   )
 }
 
