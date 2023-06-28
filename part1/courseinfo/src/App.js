40307ec824f4ec759e6e721a0372a403c23a2b4d
atayiilmaz <ataberkyiilmaz@outlook.com>
2023-06-28 22:34:29 +0300

    part1.1 challenge done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,26 +1,48 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Header = (props) => {
 
   return (
+
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h1>
+        {props.course}
+      </h1>
     </div>
+  )
+}
+
+const Content = (props) => {
+
+  return (
+
+    <div>
+      <p>
+      {props.parts} {props.exercises}
+    </p>
+    </div>
+
+  )
+}
+
+const Total = (props) => {
+
+  return (
+
+    <div>
+      <p>Number of exercises {props.exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  return (
+    <>
+      <Header course = 'Half Stack application development' />
+      <Content parts = 'Fundamentals of React' exercises = {10} />
+      <Content parts = 'Using props to pass data' exercises = {7} />
+      <Content parts = 'State of a component' exercises = {14} />
+      <Total exercises = {10+7+14} />
+    </>
   )
 }
 
