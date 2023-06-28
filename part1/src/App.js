94916cb2da4169c4436f3cbbeb8ce90e4d4a2bd1
atayiilmaz <ataberkyiilmaz@outlook.com>
2023-06-28 18:40:28 +0300

    creating components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
 
 const App = () => {
-
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
-
   return (
-    
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
-
-  );
+  )
 }
 
 export default App;
