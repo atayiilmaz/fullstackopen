f27cf8d15302861deaacb8f5dfe3e434fd20f7a8
atayiilmaz <ataberkyiilmaz@outlook.com>
2023-06-28 20:55:50 +0300

    last commit

--- part1/src/App.js
+++ part1/src/App.js
@@ -23,7 +23,7 @@ const Footer = () => {
   )
 }
 
-const App = () => {
+/*const App = () => {
   const name = 'Peter'
   const age = 10
 
@@ -36,5 +36,32 @@ const App = () => {
     </>
   )
 }
+*/
+
+/*const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+*/
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
 
 export default App;
