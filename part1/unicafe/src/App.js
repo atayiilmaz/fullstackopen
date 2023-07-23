6e710534e51342a832c45ae854d8e875c35fdc56
atayiilmaz <ataberkyiilmaz@outlook.com>
2023-07-23 22:13:34 +0300

    step 1 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,16 +6,13 @@ const Button = (props) => {
   return (
 
   <>
-    <button type='submit'>{props.name}</button>
+    <button onClick={props.handleClick} type='submit'>{props.name}</button>
   </>
 
   )
 
 }
 
-const Header = () => {
-
-}
 
 const Feedback = () => {
 
@@ -26,16 +23,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGoodButton = () => {
+
+    setGood(good + 1)
+  }
+
+  const handleNeutralButton = () => {
+
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadButton = () => {
+
+    setBad(bad + 1)
+  }
 
   return (
-    <div>
-      <h1>give feedback</h1>
-      <Button name="good"></Button>
-      <Button name="neutral"></Button>
-      <Button name="bad"></Button>
-      <h1>statistics</h1>
 
-    </div>
+    <>
+      <h1>give feedback</h1>
+      <Button name="good" handleClick={handleGoodButton} />
+      <Button name="neutral" handleClick={handleNeutralButton} />
+      <Button name="bad" handleClick={handleBadButton} />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+
+    </>
   )
 }
 
